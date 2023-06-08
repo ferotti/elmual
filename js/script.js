54ec6a9fce90ebcba82c8ce099e720ef8fd80e25
frd <ceferliferid103@gmail.com>
2023-06-08 19:34:04 +0400

    prject start

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+/* Ev tapşırığı:
+
+1) Bütün reklam şəkillərini silin (saytın sağ tərəfində)
+
+2) Filmin janrını "Komediya"-dan на "Drama" -ya dəyişin
+Posterin arxa fon şəkilini "bg.jpg" ilə dəyişin. Şəkil "img" qovluğunda yerləşir. Bunun üçün ancaq JS-dən istifadə edin.
+
+4) Baxılmış kinoların siyahısını JS ilə silin, əlifbaya görə sıralayın.
+
+5) "movieDB"-də olan kino adları ilə əvəz edirik (stili HTML-dən götürün) və əlavə edilmiş kino adları nömrələnsin */
+
+'use strict';
+
+const movieDB = {
+    movies: [
+        "logan",
+        "titanic",
+        "Bəyin oğurlanması",
+        "Xaç atası",
+        "Avatar 2"
+    ]
+};
+
+
+
+
+
+
+
+
+
+
+
